936e62cf946819323fdb21afdffbc8b3fa1d2f40
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-04-15 16:13:38 +0900

    displays primarch names

--- src/components/CharacterGrid.js
+++ src/components/CharacterGrid.js
@@ -1,11 +1,16 @@
 import React from 'react'
 
-const CharacterGrid = ({ items, isLoading}) => {
-  return (
-    <div>
-      
-    </div>
+const CharacterGrid = ({ primarchs, isLoading}) => {
+  return isLoading ? ( 
+    <h1>LOADING</h1> 
+  ) : ( 
+    <section className="cards">
+        {primarchs.default.map(primarch => (
+            <h1>{primarch.name}</h1>
+        ))}
+    </section>
   )
+  
 }
 
 export default CharacterGrid
