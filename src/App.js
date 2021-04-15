1681939873e742aa2d7a829fc47e7e51128c281c
Andrew Wallace <andrew.wallace.english@gmail.com>
2021-04-15 15:56:30 +0900

    header component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Header from "./components/Header"
+import CharacterGrid from "./components/CharacterGrid"
+import "./App.css";
+import * as primarchs from "./data/primarchs.json";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    console.log(primarchs);
+    setIsLoading(false);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <CharacterGrid 
+      isLoading={isLoading}
+      primarchs={primarchs}
+      />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../App.css"
+
+const Header = () => {
+  return (
+    // LOGO
+    <header className="center">
+      THE PRIMARCHS
+    </header>
+  )
+}
+
+export default Header
